9c73a39ff18b3c0d3ac956954023df9d6c110562
0xfisher6 <0xfisher6@gmail.com>
2024-03-28 15:36:57 +0800

    add deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig, vars } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+
+const WALLET_PRIVATE_KEY = vars.get('WALLET_PRIVATE_KEY')
+
+const accounts = WALLET_PRIVATE_KEY !== undefined ? [WALLET_PRIVATE_KEY] : []
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-};
+  solidity: '0.8.24',
+  networks: {
+    ArbSepolia: {
+      url: 'https://sepolia-rollup.arbitrum.io/rpc',
+      accounts: accounts
+    },
+    local: {
+      url: 'http://127.0.0.1:8545/',
+      accounts: accounts
+    }
+  }
+}
 
-export default config;
+export default config
--- ignition/modules/Bridge.ts
+++ ignition/modules/Bridge.ts
@@ -1,7 +1,14 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
+import { ethers } from 'hardhat'
+
+const iface = new ethers.Interface(['function initialize(bool _isEnabled)'])
 
 const BridgeModule = buildModule('BridgeModule', m => {
-  const bridge = m.contract('Bridge')
+  const bridge = m.contract('Bridge', [], {
+    value: 0n
+  })
+
+  m.send('initialize', bridge, 0n, iface.encodeFunctionData('initialize', [0x1]))
 
   return { bridge }
 })
