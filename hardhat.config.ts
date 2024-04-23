a7395abb647bc23b1dabf05c56f57e85861603bb
0xfisher6 <0xfisher6@gmail.com>
2024-04-23 15:12:40 +0800

    Deploy arb

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,32 +8,8 @@ const accounts = WALLET_PRIVATE_KEY !== undefined ? [WALLET_PRIVATE_KEY] : []
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
   networks: {
-    Sepolia: {
-      url: 'https://rpc.sepolia.org',
-      accounts: accounts
-    },
-    ArbSepolia: {
-      url: 'https://sepolia-rollup.arbitrum.io/rpc',
-      accounts: accounts
-    },
-    BlastSepolia: {
-      url: 'https://sepolia.blast.io',
-      accounts: accounts
-    },
-    ScrollSepolia: {
-      url: 'https://sepolia-rpc.scroll.io',
-      accounts: accounts
-    },
-    KromaSepolia: {
-      url: 'https://api.sepolia.kroma.network',
-      accounts: accounts
-    },
-    HoleSky: {
-      url: 'https://ethereum-holesky.publicnode.com',
-      accounts: accounts
-    },
-    Katla: {
-      url: 'https://rpc.katla.taiko.xyz',
+    Arbitrum: {
+      url: 'https://arbitrum-one-rpc.publicnode.com',
       accounts: accounts
     },
     local: {
