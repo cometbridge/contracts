c56242403b7f601167faf948550592a79c1feb28
0xfisher6 <0xfisher6@gmail.com>
2024-04-23 14:14:59 +0800

    update test token

--- contracts/GLDToken.sol
+++ contracts/GLDToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.24;
 import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract GLDToken is ERC20 {
-  constructor(uint256 initialSupply) ERC20('Gold', 'GLD') {
+  constructor(uint256 initialSupply) ERC20('wrapperd BTC', 'WBTC') {
     _mint(msg.sender, initialSupply);
   }
 }
